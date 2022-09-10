f5a04d77fae8cc12609fecae0254573c11fd384e
wishmitha8 <wishmithas@gmail.com>
2022-09-10 14:10:56 +0530

    second commit

---
+++ marks.py
@@ -0,0 +1,7 @@
+class marks:
+    name=""
+    marks=""
+    def __init__(self,name,marks):
+        self.name=name
+        self.marks=marks
+
--- pythoncode.py
+++ pythoncode.py
@@ -1,3 +1,7 @@
+from marks import Marks
+
+marks= Marks("100","A")
 print("HII")
 value=200
 print(200)
+print(marks.value)
